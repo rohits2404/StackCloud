3683c25b3cc7b07841273c83ba66b42270b7ca6f
Rohit <rohits8731@gmail.com>
2026-09-10 13:45:34 +0530

    01: Project Setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+    title: "Stack Cloud",
+    description: "A Better Way To Store, Organize And Access Your Files",
+};
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+    return (
+        <html lang="en">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                {children}
+            </body>
+        </html>
+    );
+}
