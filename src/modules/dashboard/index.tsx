3995f9666d3d2a00e3b2266d670c508af404a77c
Rohit <rohits8731@gmail.com>
2026-09-12 00:27:34 +0530

    18: Dashboard Table

--- src/modules/dashboard/index.tsx
+++ src/modules/dashboard/index.tsx
@@ -1,8 +1,21 @@
 "use client";
 
+import { ActionDropdown } from "@/components/ActionDropdown";
 import { FileCard } from "@/components/FileCard";
+import { Preview } from "@/components/Preview";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
 import { getFiles } from "@/lib/appwrite/file.actions";
 import { FILE_CARDS } from "@/lib/constants";
+import { formatDateTime, getFileSize, getFileType } from "@/lib/utils";
+import { ArrowDown } from "lucide-react";
+import { Models } from "node-appwrite";
 import { useEffect, useState } from "react";
 
 export const Dashboard = () => {
@@ -35,6 +48,62 @@ export const Dashboard = () => {
                     />
                 ))}
             </div>
+            <h1 className="text-gray-700 text-2xl font-medium mt-12">
+                Recent Files
+            </h1>
+            <Table className="mt-2">
+                <TableHeader>
+                    <TableRow className="border-b border-gray-100">
+                        <TableHead className="text-gray-500 text-base font-medium">
+                            Name
+                        </TableHead>
+                        <TableHead className="text-gray-500 text-base font-medium">
+                            Last Modified
+                        </TableHead>
+                        <TableHead className="text-gray-500 text-base font-medium">
+                            Size
+                        </TableHead>
+                        <TableHead className="text-gray-500 text-base font-medium text-right">
+                            Type
+                        </TableHead>
+                        <TableHead className="text-gray-500 text-base font-medium text-right flex justify-end items-center">
+                            <ArrowDown className="w-5 h-5" />
+                        </TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {allFiles.slice(0, 7).map((file: Models.DefaultRow) => {
+                        return (
+                            <TableRow key={file.$id} className="border-b-0">
+                                <TableCell className="font-medium flex items-center gap-4">
+                                    <Preview
+                                        type={file.type}
+                                        extension={file.extension}
+                                        url={file.url}
+                                        classNames="w-10 h-10"
+                                        imgClassNames="w-6 h-6"
+                                    />
+                                    <span className="font-medium text-gray-800">
+                                        {file.name}
+                                    </span>
+                                </TableCell>
+                                <TableCell className="font-medium text-gray-800">
+                                    {formatDateTime(file.$updatedAt)}
+                                </TableCell>
+                                <TableCell className="font-medium text-gray-800">
+                                    {getFileSize(file.size)}
+                                </TableCell>
+                                <TableCell className="text-right font-medium text-gray-800">
+                                    {getFileType(file.name).type}
+                                </TableCell>
+                                <TableCell className="flex justify-end font-medium text-gray-800 cursor-pointer">
+                                    <ActionDropdown file={file} />
+                                </TableCell>
+                            </TableRow>
+                        );
+                    })}
+                </TableBody>
+            </Table>
         </div>
     );
 };
